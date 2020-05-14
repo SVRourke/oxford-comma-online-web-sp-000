f0ad24c110d378e0034349c2affb81bb7ab1a531
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 00:19:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 2 
+    array.join(', and ')
+  elsif array.count > 2
+    array[-1] = "and " + array[-1]
+    array.join(', ')
+  else
+    array
+  end
 end
+
